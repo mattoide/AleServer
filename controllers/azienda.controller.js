064992db8262912673b1a70751f317d284546ef8
Mattia <mattiabellotti.dev@gmail.com>
2020-10-28 13:03:48 +0100

    Update azienda.controller.js

--- controllers/azienda.controller.js
+++ controllers/azienda.controller.js
@@ -14,28 +14,28 @@ var bcrypt = require("bcryptjs");
 exports.registraAzienda = (req, res) => {
 
   Azienda.create({
-    ragioneSociale: req.body.ragioneSociale,
-    //password: bcrypt.hashSync(req.body.password, 8),
-    email: req.body.email,
-    cel: req.body.cel,
-    tel: req.body.tel,
-    toponimo: req.body.toponimo,
-    indirizzo: req.body.indirizzo,
-    civico: req.body.civico,
-    provincia: req.body.provincia,
-    cap: req.body.cap,
-    apertura: req.body.apertura,
-    chiusura: req.body.chiusura,
-    descrizione: req.body.descrizione,
+    ragioneSociale: req.body.azienda.ragioneSociale,
+    //password: bcrypt.hashSync(req.body.azienda.password, 8),
+    email: req.body.azienda.email,
+    cel: req.body.azienda.cel,
+    tel: req.body.azienda.tel,
+    toponimo: req.body.azienda.toponimo,
+    indirizzo: req.body.azienda.indirizzo,
+    civico: req.body.azienda.civico,
+    provincia: req.body.azienda.provincia,
+    cap: req.body.azienda.cap,
+    apertura: req.body.azienda.apertura,
+    chiusura: req.body.azienda.chiusura,
+    descrizione: req.body.azienda.descrizione,
 
   }).then(azienda => {
 
-    if (req.body.categorie && req.body.categorie.length > 0) {
+    if (req.body.azienda.categorie && req.body.azienda.categorie.length > 0) {
 
       Categoia.findAll({
         where: {
           categoria: {
-            [Op.or]: req.body.categorie
+            [Op.or]: req.body.azienda.categorie
           }
         }
       }).then(categorie => {
@@ -46,12 +46,12 @@ exports.registraAzienda = (req, res) => {
       });
     }
 
-    if (req.body.prodotti && req.body.prodotti.length > 0) {
+    if (req.body.azienda.prodotti && req.body.azienda.prodotti.length > 0) {
 
       Prodotto.findAll({
         where: {
           prodotto: {
-            [Op.or]: req.body.prodotti
+            [Op.or]: req.body.azienda.prodotti
           }
         }
       }).then(prodotti => {
@@ -59,12 +59,12 @@ exports.registraAzienda = (req, res) => {
       });
     }
 
-    if (req.body.servizi && req.body.servizi.length > 0) {
+    if (req.body.azienda.servizi && req.body.azienda.servizi.length > 0) {
 
       Servzio.findAll({
         where: {
           servizio: {
-            [Op.or]: req.body.servizi
+            [Op.or]: req.body.azienda.servizi
           }
         }
       }).then(servizi => {
